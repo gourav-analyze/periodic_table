c04a3b039ea62f21663eb9743ce22070c8743c36
gourav batra <124765223+gourav-analyze@users.noreply.github.com>
2023-03-01 11:38:56 +0530

    Update element.sh

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
 if [[ -z $1 ]]
 then
-  echo -e "\nPlease provide an element as an argument.\n"
+  echo "Please provide an element as an argument.\n"
   exit
 fi
 
@@ -20,12 +20,12 @@ fi
 #element not in db
 if [[ -z $element ]]
 then
-  echo -e "\nI could not find that element in the database."
+  echo "I could not find that element in the database."
   exit
 fi
 
 echo $element | while IFS=" |" read an name symbol type mass mp bp 
 do
-  echo -e "\nThe element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+  echo "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
 done
  
